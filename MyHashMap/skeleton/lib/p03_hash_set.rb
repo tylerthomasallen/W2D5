a57319c19ba4d40aeb41339dd462e9dc666a8575
tylerthomasallen <tylerthomasallen@gmail.com>
2018-08-31 16:08:17 -0700

    Complete hash_set

--- MyHashMap/skeleton/lib/p03_hash_set.rb
+++ MyHashMap/skeleton/lib/p03_hash_set.rb
@@ -1,3 +1,5 @@
+
+
 class HashSet
   attr_reader :count
 
@@ -7,12 +9,28 @@ class HashSet
   end
 
   def insert(key)
+    unless self.include?(key)
+      
+      hash_val = key.hash
+      @store[hash_val % num_buckets] << key
+      @count += 1
+    end
+    
+    resize!  if self.count >= num_buckets
   end
 
   def include?(key)
+    hash_val = key.hash
+    @store[hash_val % num_buckets].include?(key)
   end
 
   def remove(key)
+    if self.include?(key)
+      
+      hash_val = key.hash
+      @store[hash_val % num_buckets].delete(key)
+      @count -= 1
+    end
   end
 
   private
@@ -26,5 +44,15 @@ class HashSet
   end
 
   def resize!
+    new_store = Array.new(num_buckets * 2) {Array.new}
+    
+    @store.each do |bucket|
+      bucket.each do |el|
+        hash_el = el.hash
+        
+        new_store[hash_el % new_store.length] << el
+      end
+    end
+    @store = new_store
   end
 end
