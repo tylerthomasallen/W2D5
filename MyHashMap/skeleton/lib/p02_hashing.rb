34354a2bd04756572188b6ecd064d135fcc03049
tylerthomasallen <tylerthomasallen@gmail.com>
2018-08-31 15:26:07 -0700

    Complete hashing functions

--- MyHashMap/skeleton/lib/p02_hashing.rb
+++ MyHashMap/skeleton/lib/p02_hashing.rb
@@ -4,11 +4,22 @@ end
 
 class Array
   def hash
+    if self == self.sort
+      (self.length + 10).hash
+    else
+      (self.length + 20).hash
+    end
   end
 end
 
 class String
   def hash
+    alphabet = ('a'..'z').to_a + ('A'..'Z').to_a
+    result = ''
+    self.each_char do |char|
+      result += alphabet.find_index(char).to_s
+    end
+    result.to_i.hash
   end
 end
 
@@ -16,6 +27,6 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    self.keys.sort.hash + self.values.sort.hash
   end
 end
